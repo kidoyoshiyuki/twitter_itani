8fc8ef805e743fdc88e164cfd32d22f0d826006c
yuka itani <yukapick@gmail.com>
2021-06-29 20:43:08 +0900

    ポップオーバー、いいね！カウント実装

--- Views/home.php
+++ Views/home.php
@@ -101,6 +101,12 @@ if ($diff_sec < 60) {
     <!--Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="<?php echo HOME_URL;?>Views/css/style.css" rel="stylesheet">
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+    <!-- いいね！いいね！js -->
+    <script src="<?php echo HOME_URL; ?>Views/js/like.js" defer></script>
     <title>ホーム画面 / Twitterクローン</title>
 </head>
 <body class="home">
@@ -114,7 +120,7 @@ if ($diff_sec < 60) {
                     <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-notification.svg" alt=""></a></li>
                     <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-profile.svg" alt=""></a></li>
                     <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
-                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt=""></li>
+                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='plofile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>" data-bs-html="true"></li>
                 </ul>
             </div>
         </div>
@@ -166,7 +172,7 @@ if ($diff_sec < 60) {
                                 <?php endif; ?>
 
                                 <div class="icon-list">
-                                    <div class="like">
+                                    <div class="like js-like" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
                                         <?php
                                         if(isset($view_tweet['like_id'])){
                                             // いいね！してる場合
@@ -176,7 +182,7 @@ if ($diff_sec < 60) {
                                         }
                                         ?>
                                     </div>
-                                    <div class="like-count"><?php echo htmlspecialchars($view_tweet['like_count']);?></div>
+                                    <div class="like-count js-like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
                                 </div>
                             </div>
                         </div>
@@ -186,6 +192,13 @@ if ($diff_sec < 60) {
             <?php endif;?>
         </div>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function(){
+            $('.js-popover').popover({
+                container:'body'
+            })
+        },false);
+    </script>
 </body>
  
 </html>
