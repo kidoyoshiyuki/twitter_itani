c4b838c9de3f8c7fb4a9efcc16e21e564a8c078b
Yuka <yuka@YukanoMacBook-ea.local>
2022-01-16 12:19:21 +0900

    config

--- config.php
+++ config.php
@@ -6,7 +6,14 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL', '/twitterclone/');
 // データーベースの接続情報
-define('DB_HOST','localhost');
-define('DB_USER','dbuser');
-define('DB_PASSWORD','Yuka@0204');
-define('DB_NAME','twitter_clone');
+// define('DB_HOST','localhost');
+define('DB_HOST',getenv('DB_HOST'));
+
+// define('DB_USER','dbuser');
+define('DB_USER',getenv('DB_USER'));
+
+// define('DB_PASSWORD','Yuka@0204');
+define('DB_PASSWORD',getenv('DB_PASSWORD'));
+
+// define('DB_NAME','twitter_clone');
+define('DB_NAME',getenv('DB_NAME'));
